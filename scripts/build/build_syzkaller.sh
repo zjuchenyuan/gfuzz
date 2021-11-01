cc7d311176f4817aabca3335228ba8cbee365a44
zjuchenyuan <chenyuan@zju.edu.cn>
2021-11-01 16:53:08 +0800

    [docs] update docs and move patch files

--- scripts/build/build_syzkaller.sh
+++ scripts/build/build_syzkaller.sh
@@ -3,5 +3,11 @@ ROOT=`git rev-parse --show-toplevel`
 git clone https://github.com/google/syzkaller
 cd syzkaller
 git checkout 9d751681c
-git apply $ROOT/0001-gfuzz-gfuzz-changes.patch
+cd ..
+cp -r syzkaller origin_syzkaller
+cd syzkaller
+git apply $ROOT/scripts/build/gfuzz-changes.patch
+make
+cd ../origin_syzkaller
+git apply $ROOT/scripts/build/origin_syzkaller.patch
 make
